861f74dbbce18774285c481233732d65133f9e10
andrews <andrews@wdhouse.com.br>
2020-09-15 09:52:50 -0300

    quase

--- ArrayList.py
+++ ArrayList.py
@@ -8,23 +8,43 @@ class List:
         self.list = [None]*size
 
     def put_list(self,position,product):
+        count_times = 0
         count_none = 0
         for element in range(0,self.size):
             if (self.list[element] == None): 
                 print('element: '+str(element))
                 count_none = count_none + 1
+                print('count: '+str(count_none))
                 position_none = element
-                if (count_none == self.size):
-                    self.list[0] = product    
+        for element_list in range(0,position-1):
+            # print('range: '+str(element_list))
+            # print('self.list: '+str(self.list[element_list]))
+            if self.list[element_list] == None:
+                # print('element list '+str(element_list))
+                # print('position '+str(position))                
+                # print('veio aqui')                    
+                if (count_times == 0):
+                    self.list[element_list] = product
+                count_times = count_times + 1
+                # funcionando ate aqui
+            elif (self.list[element_list] != None):
+                print('ta caindo aqui')
+                for last_list in self.list[position::-1]:
+                    print('last list: '+str(last_list))
+                    print('last SELF list: '+str(self.list[last_list]))
+                    self.list[last_list] = self.list[last_list-1]
+                #self.list[position] = product
+            
 
-                else:
+
+                # if (count_none != self.size):
                     # if (position > position_none):
                     #     self.list[position_none] = product     
 
-                    if (position < position_none):
-                        if (self.list[position] != None):
-                            for last_list in range(position-1, self.size,-1):
-                                self.list[last_list] = self.list[last_list-1]
+                    # if (position < position_none):
+                    #     if (self.list[position] != None):
+                    #         for last_list in range(position-1, self.size,-1):
+                    #             self.list[last_list] = self.list[last_list-1]
                             
 
 
--- teste.py
+++ teste.py
@@ -1,22 +1,22 @@
-# Feito em conjunto por: Andrews Dutra, Mateus Marques e Guilherme Steglich
-class Product:
-    def __init__(self,name):
-        self.name = name
+# # Feito em conjunto por: Andrews Dutra, Mateus Marques e Guilherme Steglich
+# class Product:
+#     def __init__(self,name):
+#         self.name = name
 
-class List:
-    def __init__(self, size):
-        self.list = [None]*size
+# class List:
+#     def __init__(self, size):
+#         self.list = [None]*size
 
-    def put_list(self,position,product):
-        count_size = 0
-        for size_list in self.list:
-            count_size = count_size + 1
-        # print (count_size)
-        # print(self.list)
-        for element in range(0,count_size,-1): 
-            if (self.list[element] == None):           
-                aux_index_element = element
-                print('auxiliar'+str(aux_index_element))
+#     def put_list(self,position,product):
+#         count_size = 0
+#         for size_list in self.list:
+#             count_size = count_size + 1
+#         # print (count_size)
+#         # print(self.list)
+#         for element in range(0,count_size,-1): 
+#             if (self.list[element] == None):           
+#                 aux_index_element = element
+#                 print('auxiliar'+str(aux_index_element))
             
             
             
@@ -24,18 +24,18 @@ class List:
 
 
 
-            # if self.list[element] == None:
-            #     aux_index_element = element
-            #     if aux_index_element > position:
-            #         for last_list in range(position-1, count_size,-1):
-            #             self.list[last_list] = self.list[last_list-1]
-            #             print(self.list)
-            #         if (self.list[position-1] == None):
-            #             self.list[position-1] = product
-            #         else:
-            #             for last_list in range(position-1, count_size,-1):
-            #                 self.list[last_list] = self.list[last_list-1]
-                # elif aux_index_element < position:
+#             # if self.list[element] == None:
+#             #     aux_index_element = element
+#             #     if aux_index_element > position:
+#             #         for last_list in range(position-1, count_size,-1):
+#             #             self.list[last_list] = self.list[last_list-1]
+#             #             print(self.list)
+#             #         if (self.list[position-1] == None):
+#             #             self.list[position-1] = product
+#             #         else:
+#             #             for last_list in range(position-1, count_size,-1):
+#             #                 self.list[last_list] = self.list[last_list-1]
+#                 # elif aux_index_element < position:
                     
 
 
@@ -48,29 +48,33 @@ class List:
                     
 
                 
-        # [none,a,C,b]
+#         # [none,a,C,b]
 
 
-list = List(int(input('Qual o tamanho da sua lista? ')))
-iniciar = True
-while iniciar:
-    # print('Lista atualizada: ',list.exibirlista())
-    print('0 - Sair do programa')
-    print('1 - Inserir produto a lista.')
-    print('2 - Remover produto da lista.')
-    print('3 - Localizar produto na lista.')
-    print('4 - Limpar lista.')
-    operacao = int(input('Qual operação deseja realizar? '))
-    if (operacao == 0):
-        iniciar = False
-    if (operacao == 1):
-        product = Product(input("Qual o nome do produto: "))
-        list.put_list(int(input("Digite a posição do produto: ")),product.name)
-    # if (operacao == 2):
-    #     posicao = int(input("Digite a posição do produto que você quer remover: "))
-    #     lista.removerproduto(posicao)
-    # if (operacao == 3):
-    #     produto = input("Digite o nome do produto que você quer encontrar: ")
-    #     print("O produto está na posição: "+str(lista.posicaoproduto(produto)))
-    # if (operacao == 4):
-    #     lista.limparlista()
+# list = List(int(input('Qual o tamanho da sua lista? ')))
+# iniciar = True
+# while iniciar:
+#     # print('Lista atualizada: ',list.exibirlista())
+#     print('0 - Sair do programa')
+#     print('1 - Inserir produto a lista.')
+#     print('2 - Remover produto da lista.')
+#     print('3 - Localizar produto na lista.')
+#     print('4 - Limpar lista.')
+#     operacao = int(input('Qual operação deseja realizar? '))
+#     if (operacao == 0):
+#         iniciar = False
+#     if (operacao == 1):
+#         product = Product(input("Qual o nome do produto: "))
+#         list.put_list(int(input("Digite a posição do produto: ")),product.name)
+#     # if (operacao == 2):
+#     #     posicao = int(input("Digite a posição do produto que você quer remover: "))
+#     #     lista.removerproduto(posicao)
+#     # if (operacao == 3):
+#     #     produto = input("Digite o nome do produto que você quer encontrar: ")
+#     #     print("O produto está na posição: "+str(lista.posicaoproduto(produto)))
+#     # if (operacao == 4):
+#     #     lista.limparlista()
+
+lista = [1,2,3,4,5,6,7]
+for i in lista[::-1]:
+    print(i)
