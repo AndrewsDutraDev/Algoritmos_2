6f30ea3c8c74002e4628c16185da4959bb67634c
Mateus Marques <mateusmarquesdc@gmail.com>
2020-11-09 01:55:49 -0300

    implementação 10/10 das listas encadeadas, assim como a comparação da mesma.

--- Pilhas/TAD_Encadeamento.py
+++ Pilhas/TAD_Encadeamento.py
@@ -1,35 +1,58 @@
-from config import Nodo
 
 class PilhaEnc:
     def __init__(self):
-        self.topo = None
-    
-    def vazia(self):
-        boolean = True
-        if (self.topo != None):
-            boolean = False
-        return boolean
+        self.vector = [None]
+        self.top = -1
+        self.base = 0
 
     def insert_top(self, value):
-        new = Nodo(value)
-        if (not self.vazia()):
-            new.next = self.topo
-        self.topo = new
-    
+        counter = 0
+        for x in self.vector:
+            counter += 1
+        new_size = counter + 1
+        vector_helper = self.vector
+        self.vector = [None] * new_size
+        for x in range(0, counter):
+            self.vector[x] = vector_helper[x]
+        self.vector[self.top] = ('<--', value)
+            
     def remove_top(self):
-        if (not self.vazia()):
-            aux = self.topo
-            self.topo = aux.next
-            del aux
+        counter = 0
+        for x in self.vector:
+            counter += 1
+        if counter == 1:
+            return
+        else:
+            new_size = counter - 1
+            vector_helper = self.vector
+            self.vector = [None] * new_size
+            for x in range(0, counter - 1):
+                self.vector[x] = vector_helper[x]
 
     def query(self):
-        if (not self.vazia()):
-            return self.topo.value
-            
+        if self.vector[self.base] == self.vector[self.top]:
+            return
+        else:
+            return self.vector[self.top][1]
+
     def destroy(self):
-        while (not self.vazia()):
-            self.remove_top()
-
+        self.vector = [None]
     
-        
-
+    def get_list(self):
+        return self.vector
+    
+    def compare(self, list_one, list_two):
+        counter_listone = 0
+        for x in list_one:
+            counter_listone += 1
+        counter_listtwo = 0
+        for x in list_two:
+            counter_listtwo += 1
+        equallity = True
+        if counter_listone != counter_listtwo:
+            equallity = False
+        else:
+            for x in range(0, counter_listone):
+                if list_one[x] != list_two[x]:
+                    equallity = False
+        return equallity
--- Pilhas/main.py
+++ Pilhas/main.py
@@ -16,6 +16,7 @@ while iniciar:
     print('2 - Remover nó do topo da pilha.')
     print('3 - Consultar nó do topo da pilha')
     print('4 - Destruir pilha')
+    print('5 - Comparar lista')
     operation = int(input('Qual operação deseja realizar? '))
     if (operation == 0):
         iniciar = False
@@ -29,7 +30,7 @@ while iniciar:
     if (operation == 4):
         lista.destroy()
     if (operation == 5):
-        print('Você está na segunda lista')
+        print('Você está na segunda lista ')
         if (type_list == 1): 
             times = int(input('Quantos valores você quer na lista secundária? '))
             lista_sec = Pilha(times)
@@ -37,3 +38,15 @@ while iniciar:
                 value = input("Qual é o "+ str(i+1) +"° valor da lista: ")
                 lista_sec.insert_top(value)
             print("As listas são iguais? "+str(lista.compare(lista.get_list(), lista_sec.get_list())))
+        if (type_list == 2):
+            lista_sec = PilhaEnc()
+            new_value = True
+            while new_value:
+                x = int(input('Deseja colocar uma valor na lista? 1 para SIM e 2 para NÃO: '))
+                if x == 1:
+                    value_list_sec = input('Qual o valor? ')
+                    lista_sec.insert_top(value_list_sec)
+                if x == 2:
+                    new_value = False
+            print("As listas são iguais? "+str(lista.compare(lista.get_list(), lista_sec.get_list())))
+            
